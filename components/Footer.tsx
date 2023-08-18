1e86ebcf44d99fb86eddabf2f4001752aee1c9a3
thecil <thecilsurf@gmail.com>
2023-08-18 02:32:40 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,15 +6,8 @@ const Footer: React.FC = () => {
   return (
     <>
       <footer className="flex bg-gray-200 dark:bg-gray-800 p-4 justify-between">
-        <div className="flex space-x-2">
-          <p className="font-bold">TECH CHALLENGE</p>
-          <p className="text-center">
-            Full Stack Developer Rather Labs - thecil
-          </p>
-        </div>
-        <div>
-          github repo here
-        </div>
+        <p className="font-bold">TECH CHALLENGE</p>
+        <p className="text-center">Full Stack Developer Rather Labs - thecil</p>
       </footer>
     </>
   );
